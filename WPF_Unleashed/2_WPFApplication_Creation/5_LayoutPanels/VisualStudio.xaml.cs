955b8565e0bca0850588381b7cbadf955c70e277
fan123777 <andrey.fonaskov@gmail.com>
2016-02-17 11:00:43 +0200

    chapter5 in progress

---
+++ WPF_Unleashed/2_WPFApplication_Creation/5_LayoutPanels/VisualStudio.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF_Unleashed._2_WPFApplication_Creation._5_LayoutPanels
+{
+    /// <summary>
+    /// Interaction logic for VisualStudio.xaml
+    /// </summary>
+    public partial class VisualStudio : Window
+    {
+        public VisualStudio()
+        {
+            InitializeComponent();
+        }
+    }
+}
