377fda6aa8d7eb0fb1301119df8b7755961f4691
fan123777 <andrey.fonaskov@gmail.com>
2016-02-17 15:59:42 +0200

    chapter5

---
+++ WPF_Unleashed/2_WPFApplication_Creation/5_LayoutPanels/ScrollView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF_Unleashed._2_WPFApplication_Creation._5_LayoutPanels
+{
+    /// <summary>
+    /// Interaction logic for ScrollView.xaml
+    /// </summary>
+    public partial class ScrollView : Window
+    {
+        public ScrollView()
+        {
+            InitializeComponent();
+        }
+    }
+}
