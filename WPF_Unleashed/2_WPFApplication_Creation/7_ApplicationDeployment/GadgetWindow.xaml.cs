de64c2be5c0fe8820292a34ffb367a866a2bd297
fan123777 <andrey.fonaskov@gmail.com>
2016-03-25 14:37:19 +0200

    chapter7

---
+++ WPF_Unleashed/2_WPFApplication_Creation/7_ApplicationDeployment/GadgetWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF_Unleashed._2_WPFApplication_Creation._7_ApplicationDeployment
+{
+    /// <summary>
+    /// Interaction logic for GadgetWindow.xaml
+    /// </summary>
+    public partial class GadgetWindow : Window
+    {
+        public GadgetWindow()
+        {
+            InitializeComponent();
+        }
+
+        void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+        void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
