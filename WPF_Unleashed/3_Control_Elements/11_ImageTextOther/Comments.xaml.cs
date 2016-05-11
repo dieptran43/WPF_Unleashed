77eac8a758d5a83487e40d9f715c3508a78669d6
fan123777 <andrey.fonaskov@gmail.com>
2016-05-11 17:47:56 +0300

    chapter11

---
+++ WPF_Unleashed/3_Control_Elements/11_ImageTextOther/Comments.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Annotations;
+using System.Windows.Annotations.Storage;
+using System.IO;
+
+namespace WPF_Unleashed._3_Control_Elements._11_ImageTextOther
+{
+    /// <summary>
+    /// Interaction logic for Comments.xaml
+    /// </summary>
+    public partial class Comments : Window
+    {
+        FileStream stream;
+
+        public Comments()
+        {
+            InitializeComponent();
+        }
+
+        protected void OnInitialized(object sender, EventArgs e)
+        {
+            // Включить и загрузить комментарии 
+            AnnotationService service = AnnotationService.GetService(reader);
+            if (service == null)
+            {
+                stream = new FileStream("storage.xml", FileMode.OpenOrCreate);
+                service = new AnnotationService(reader);
+                AnnotationStore store = new XmlStreamStore(stream);
+                store.AutoFlush = true;
+                service.Enable(store);
+            }
+        }
+        protected void OnClosed(object sender, EventArgs e)
+        {
+            // Выключить и сохранить комментарии
+            AnnotationService service = AnnotationService.GetService(reader);
+            if (service != null && service.IsEnabled)
+            {
+                service.Disable();
+                stream.Close();
+            }
+        }
+    }
+}
---
+++ WPF_Unleashed/3_Control_Elements/11_ImageTextOther/TextInk.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF_Unleashed._3_Control_Elements._11_ImageTextOther
+{
+    /// <summary>
+    /// Interaction logic for TextInk.xaml
+    /// </summary>
+    public partial class TextInk : Window
+    {
+        public TextInk()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            // ... Get control that raised this event.
+            var textBox = sender as TextBox;
+            // ... Change Window Title.
+            this.Title = textBox.Text +
+            "[Length = " + textBox.Text.Length.ToString() + "]";
+        }
+    }
+}
