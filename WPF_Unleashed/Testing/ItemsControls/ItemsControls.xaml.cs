2278c73deee9c289a926df1db2eede0e62069e0c
fan123777 <andrey.fonaskov@gmail.com>
2016-06-07 18:00:37 +0300

    testing in progress

---
+++ WPF_Unleashed/Testing/ItemsControls/ItemsControls.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF_Unleashed.Testing.ItemsControls
+{
+    /// <summary>
+    /// Interaction logic for ItemsControls.xaml
+    /// </summary>
+    public partial class ItemsControls : Window
+    {
+        public ItemsControls()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ListBox window = new ListBox();
+            window.Show();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            ItemsPanel window = new ItemsPanel();
+            window.Show();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            Selectors window = new Selectors();
+            window.Show();
+        }
+    }
+}
---
+++ WPF_Unleashed/Testing/ItemsControls/Selectors.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF_Unleashed.Testing.ItemsControls
+{
+    /// <summary>
+    /// Interaction logic for Selectors.xaml
+    /// </summary>
+    public partial class Selectors : Window
+    {
+        public Selectors()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ComboBox window = new ComboBox();
+            window.Show();
+        }
+    }
+}
